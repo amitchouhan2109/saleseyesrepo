7544548261bc865e2abde727bff3db21995b3c38
Diksha <diksha1995@gmail.com>
2020-08-19 23:34:45 +0530

    not running

--- android/app/src/main/java/com/taskeriumcp/MainActivity.java
+++ android/app/src/main/java/com/taskeriumcp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.saleseyes;
+package com.taskeriumcp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "saleseyes";
+    return "TaskeriumCP";
   }
 }
